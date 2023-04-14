5df07621866d86cbfedce6d1de6c72cc1b6c943c
Stéphane Laurent <laurent_step@yahoo.fr>
2023-04-14 15:31:42 +0200

    DESCRIPTION

--- inst/benchmarks/benchmarks.R
+++ inst/benchmarks/benchmarks.R
@@ -1,6 +1,7 @@
 library(intmap)
 library(microbenchmark)
 
+# erase
 keys <- 1:10000
 values <- as.list(letters[1 + keys%%26])
 
@@ -34,6 +35,54 @@ flist <- function() {
 
 microbenchmark(
   intmap = fintmap(), # faster
+  List   = flist(),
+  times = 5
+)
+
+# insert again
+items <- (-5000):15000
+items_char <- as.character(items)
+
+fintmap <- function() {
+  for(. in 1:10000) {
+    i <- sample(items, size = 1L)
+    imap$insert(i, "X", replace = TRUE)
+  }
+}
+
+flist <- function() {
+  for(. in 1:10000) {
+    i <- sample(items_char, size = 1L)
+    values[[i]] <- "X"
+  }
+}
+
+microbenchmark(
+  intmap = fintmap(), # faster
+  List   = flist(),
+  times = 5, 
+  setup = {
+    keys <- 1:10000
+    values <- as.list(letters[1 + keys%%26])
+    imap <- intmap$new(keys, values)
+  }
+)
+
+# extract 
+keys <- 1:1000
+values <- as.list(letters[1 + keys%%26])
+imap <- intmap$new(keys, values)
+
+fintmap <- function() {
+  imap$extract(500:1500, inplace = FALSE, bydeleting = TRUE)
+}
+
+flist <- function() {
+  newvalues <- values[as.character(500:1500)]
+}
+
+microbenchmark(
+  intmap = fintmap(), # slower
   List   = flist(),
   times = 5
 )
